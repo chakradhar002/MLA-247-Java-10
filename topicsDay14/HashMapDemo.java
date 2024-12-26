c4ce77292edf458488d68002e36020abf8759a0d
unknown <mantha747@gmail.com>
2024-12-26 14:45:14 +0530

    hasmap

---
+++ topicsDay14/HashMapDemo.java
@@ -0,0 +1,23 @@
+package chapter6;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		HashMap<Integer,String> mapdemo = new HashMap<Integer,String>();
+		
+		mapdemo.put(1, "ram");
+		mapdemo.put(2, "sham");
+		mapdemo.put(3, "tom");
+		mapdemo.put(4, "ravi");
+	////// Access a value by keySystem.out.println(mapdemo);	
+	
+		// Iterate through the HashMap/// Remove a key-value pair	// Check if a key exists
+		 // Replace a value
+	}
+
+}
