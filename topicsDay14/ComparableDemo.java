70278f7b49a49b9242998d44e2adb10af9ce0d36
unknown <mantha747@gmail.com>
2024-12-26 16:35:24 +0530

    comparable demo

--- topicsDay14/ComparableDemo.java
+++ topicsDay14/ComparableDemo.java
@@ -15,11 +15,10 @@ negative integer, if the current object is less than the specified object.
 zero, if the current object is equal to the specified object.
  **/
 
-class Student  implements Comparable <Student>{
-	
-	
+class Student implements Comparable<Student> {
+
 	public Student(int id, String name, int marks) {
-	
+
 		this.id = id;
 		this.name = name;
 		this.marks = marks;
@@ -27,14 +26,26 @@ class Student  implements Comparable <Student>{
 
 	private int id;
 	private String name;
-	private int marks;
-	
+	private int currentstudentmarks;
+
 	//
 
 	@Override
-	public int compareTo(Student  other) {
-		
-		return Integer.compare(this.marks,other.marks);
+	public int compareTo(Student other) {
+
+		if (currentstudentmarks == other.currentstudentmarks) {
+
+			return 0;
+
+		} else if (currentstudentmarks > other.currentstudentmarks) {
+
+			return 1;
+		} else if (currentstudentmarks < other.currentstudentmarks) {
+
+			return -1;
+		}
+		return id;
+
 	}
 
 	public int getId() {
@@ -60,46 +71,41 @@ class Student  implements Comparable <Student>{
 	public void setMarks(int marks) {
 		this.marks = marks;
 	}
+
 	@Override
-    public String toString() {
-        return "Student{id=" + id + ", name='" + name + "', marks=" + marks + "}";
-    }	
+	public String toString() {
+		return "Student{id=" + id + ", name='" + name + "', marks=" + marks + "}";
+	}
 
 }
 
-
 public class ComparableDemo {
-	
+
 	public static void main(String args[]) {
-		
-		List<Student> students  = new ArrayList<Student>();
-		
-		students.add(new Student(1,"ram",87));
-		students.add(new Student(2,"sham",45));
-		students.add(new Student(3,"tom",34));
-		
-		
-		//printing before sorting
-		
-		for(Student names : students) {
-			
+
+		List<Student> students = new ArrayList<Student>();
+
+		students.add(new Student(1, "ram", 87));
+		students.add(new Student(2, "sham", 45));
+		students.add(new Student(3, "tom", 34));
+
+		// printing before sorting
+
+		for (Student names : students) {
+
 			System.out.println(names);
 		}
-		
-		//sort the students using collections.sort
-		
+
+		// sort the students using collections.sort
+
 		Collections.sort(students);
-		
-	//printing after sorting
-		
-		for(Student names : students) {
-			
+
+		// printing after sorting
+
+		for (Student names : students) {
+
 			System.out.println(names);
 		}
 	}
-
-
-
-
 
 }
