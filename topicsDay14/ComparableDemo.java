397ab1a77cf96e89c3c2edabaf52ac87d0d94aab
unknown <mantha747@gmail.com>
2024-12-26 16:07:17 +0530

    comparable demo

--- topicsDay14/ComparableDemo.java
+++ topicsDay14/ComparableDemo.java
@@ -5,7 +5,8 @@ import java.util.Collections;
 import java.util.List;
 
 /*
- * 
+ * Java Comparable interface is used to order the objects of the user-defined class. This interface is found in java.lang package and contains only one method named compareTo(Object). It provides a single sorting sequence only, i.e., you can sort the elements on the basis of single data member only.
+ *  For example, it may be rollno, name, age or anything else.
  * compareTo(Object obj) method
 public int compareTo(Object obj): It is used to compare the current object with the specified object. It returns
 
