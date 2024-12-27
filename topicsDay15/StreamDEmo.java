0f043884cd694d51273b25aaa401532f30472073
unknown <mantha747@gmail.com>
2024-12-27 11:25:39 +0530

    stream demo

---
+++ topicsDay15/StreamDEmo.java
@@ -0,0 +1,20 @@
+package chapter7;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamDEmo {
+//finding even or oadd number
+	public static void main(String[] args) {
+	//declare list in collections 
+
+		
+ List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9);
+ List<Integer> resultevennumbers = numbers.stream().filter(n->n%2==0).collect(Collectors.toList());
+ System.out.println(resultevennumbers);
+	}
+
+	
+	
+}
